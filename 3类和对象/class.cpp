f97a7445aaa80a8f6c1e2e0f16a9dce48746d0f1
zb <1662308219@qq.com>
2025-02-16 17:30:14 +0800

    update

--- 3类和对象/class.cpp
+++ 3类和对象/class.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Maker // 1.把属性和方法放到类中，赋予权限
 {
-private:
+public:
     int id;
     string name;
 
@@ -28,6 +28,17 @@ void test()
     m.set("sb", 1);
     m.printMaker();
 }
+
+class Son : public Maker
+{
+    void func()
+    {
+        a = 20; // 子类的类内可以访问父类的保护权限的成员
+    }
+};
+
+// 类内没有权限之分
+
 int main()
 {
     test();
