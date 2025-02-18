60ca72057526d7591cb639791b672d487d816fac
zb <1662308219@qq.com>
2025-02-18 23:25:50 +0800

    update

---
+++ 3类和对象/cube.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+class cube
+{
+private:
+    int ml;
+    int mw;
+    int mh;
+
+public:
+    void init()
+    {
+        ml = 0;
+        mw = 0;
+        mh = 0;
+    }
+    void setl(int l)
+    {
+        ml = l;
+    }
+    void setw(int w)
+    {
+        mw = w;
+    }
+    void seth(int h)
+    {
+        mh = h;
+    }
+
+    // get
+    int getl()
+    {
+        return ml;
+    }
+    int getw()
+    {
+        return mw;
+    }
+    int geth()
+    {
+        return mh;
+    }
+
+    int caculateS()
+    {
+        return 2 * (ml * mw + ml * mh + mw * mh);
+    }
+
+    int caculateV()
+    {
+        return ml * mw * mh;
+    }
+
+    bool compareCube(cube &cube)
+    {
+        if (getl() == cube.getl() && getw() == cube.getw() && geth() == cube.geth())
+            return true;
+        else
+            return false;
+    }
+};
+
+int main()
+{
+    cube c1, c2;
+    c1.init();
+    c2.init();
+
+    c1.setl(10);
+    c1.setw(20);
+    c1.seth(30);
+    c2.setl(10);
+    c2.setw(20);
+    c2.seth(30);
+
+    if (c1.compareCube(c2))
+    {
+        cout << "==========" << endl;
+    }
+    return 0;
+}
